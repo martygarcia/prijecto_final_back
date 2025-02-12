663355d2c381ac5b2acefc3e6870f9f023c6bb53
martygarcia <amartinezgarcia277@gmail.com>
2025-02-12 10:36:50 +0100

    prueba

--- dist/src/db-conncection.js
+++ dist/src/db-conncection.js
@@ -2,12 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.query = void 0;
 var pg_1 = require("pg");
+// const pool = new Pool({
+//     user: 'postgres',
+//     password: 'a',
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'Projecto_final'
+// });
+var connectionString = 'postgresql://proyecto_final_c467_user:DCM0TNCwjZ0tfP0qky0OTIwThOWRyqfT@dpg-cum64u0gph6c73dbu140-a.oregon-postgres.render.com/proyecto_final_c467';
 var pool = new pg_1.Pool({
-    user: 'postgres',
-    password: 'a',
-    host: 'localhost',
-    port: 5432,
-    database: 'Projecto_final'
+    connectionString: connectionString,
 });
 function query(text) {
     return pool.query(text);
