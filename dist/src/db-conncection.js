c445a8171e1221946ef2ce606b1a46234aa0d8aa
martygarcia <amartinezgarcia277@gmail.com>
2025-02-16 02:15:12 -0300

    prueba de add_ medals

--- dist/src/db-conncection.js
+++ dist/src/db-conncection.js
@@ -2,17 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.query = void 0;
 var pg_1 = require("pg");
-var pool = new pg_1.Pool({
-    user: 'postgres',
-    password: 'a',
-    host: 'localhost',
-    port: 5432,
-    database: 'pokemon_game'
-});
-// const connectionString = 'postgresql://proyecto_final_c467_user:DCM0TNCwjZ0tfP0qky0OTIwThOWRyqfT@dpg-cum64u0gph6c73dbu140-a/proyecto_final_c467'
 // const pool = new Pool({
-//     connectionString,
-// })
+//     user: 'postgres',
+//     password: 'a',
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'pokemon_game'
+// });
+var connectionString = 'postgresql://proyecto_final_c467_user:DCM0TNCwjZ0tfP0qky0OTIwThOWRyqfT@dpg-cum64u0gph6c73dbu140-a/proyecto_final_c467';
+var pool = new pg_1.Pool({
+    connectionString: connectionString,
+});
 function query(text) {
     return pool.query(text);
 }
